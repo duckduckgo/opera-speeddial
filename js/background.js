c20a99a6e9e6f3df6b77a4858b189a7c6cd6ac01
Robert Picard <rpicard@live.com>
2012-03-30 21:54:43 -0400

    still working...

--- js/background.js
+++ js/background.js
@@ -1,21 +1,26 @@
 window.addEventListener('load', function() {
 
-    function getQuery() {
-
+    $.getJSON('http://en.wikipedia.org/w/api.php?callback=?',
+    {
+        action: "query",
+        list: "random",
+        rnnamespace: "0",
+        rnlimit: "1",
+        format: "json"
+    },
+    function(data) {
+    
         var title = "testing";
     
         $("#searchBox").val(title);
 
         if (opera.contexts.speeddial) {
             var url = "http://duckduckgo.com/?q=" + encodeURIComponent(title);
-            var sd = opera.contexts.speeddial;
+         var sd = opera.contexts.speeddial;
 
             sd.url = url;
         }
-    
-        return 0;
-    }
 
-    getQuery();
+    });
 
 }, false);
