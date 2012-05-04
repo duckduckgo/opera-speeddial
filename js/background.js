fa0a530ff63bc78dd1fe75361f28c785de0741f9
Robert Picard <mail@robert.io>
2012-05-04 17:02:33 -0400

    Changing function to show random ZCI rather than random search query

--- js/background.js
+++ js/background.js
@@ -1,26 +1,47 @@
 window.addEventListener('load', function() {
 
-    $.getJSON('http://en.wikipedia.org/w/api.php?callback=?',
-    {
-        action: "query",
-        list: "random",
-        rnnamespace: "0",
-        rnlimit: "1",
-        format: "json"
-    },
-    function(data) {
-    
-        var title = data.query.random[0].title;
-    
-        $("#searchBox").val(title);
+    function setTitle() {
+        $.getJSON('http://en.wikipedia.org/w/api.php?callback=?',
+            {
+                action: "query",
+                list: "random",
+                rnnamespace: "0",
+                rnlimit: "1",
+                format: "json"
+            },
+            function(data) {
+                title = data.query.random[0].title;
+                $("#header").html(title);
+                setAbstract(title);
+                setOperaContexts(title);
+            }
+        );
+    }
 
+    function setAbstract(title) {
+        $.getJSON('http://api.duckduckgo.com/?callback=?',
+            {
+                q: title,
+                format: "json",
+                no_redirect: 1,
+                skip_disambig: 1,
+            },
+            function(data) {
+                abstract = data.AbstractText;
+                $("#zci").html(abstract);
+            }
+        );
+    }
+
+    function setOperaContexts(title) {    
         if (opera.contexts.speeddial) {
-            var url = "http://duckduckgo.com/?q=" + encodeURIComponent(title);
+            var url = "http://duckduckgo.com/?q=" + title;
             var sd = opera.contexts.speeddial;
 
             sd.url = url;
         }
+    }
 
-    });
+    setTitle();
 
 }, false);
