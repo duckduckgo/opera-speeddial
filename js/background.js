37040521252094dfb2156a6946ed16f27458f896
Robert Picard <mail@robert.io>
2012-05-07 13:11:13 -0400

    Lengthen interval (to 10,000 ms), change logo, update speed dial context title with current search term, and fix bug where it wouldn't look for another title if there wasn't an abstract

--- js/background.js
+++ js/background.js
@@ -5,36 +5,34 @@ window.addEventListener('load', function() {
             {
                 action: "query",
                 list: "random",
-                rnnamespace: "0",
-                rnlimit: "1",
+                rnnamespace: 0,
+                rnlimit: 1,
+                redirects: 1,
                 format: "json"
             },
             function(data) {
+                
                 title = data.query.random[0].title;
-                getAbstract(title);
-                setOperaContexts(title);
-            }
-        );
-    }
 
-    function getAbstract(title) {
-        $.getJSON('http://api.duckduckgo.com/?callback=?',
-            {
-                q: title,
-                format: "json",
-                no_redirect: 1,
-                skip_disambig: 1,
-            },
-            function(data) {
-                abstract = data.AbstractText;
-                if (abstract){
-                    $("#header").html(title);
-                    $("#zci").html(abstract);
-                    $("#content").fadeIn('slow');
-                }
-                else {
-                    getTitle();
-                }
+                $.getJSON('http://api.duckduckgo.com/?callback=?',
+                    {
+                        q: title,
+                        format: "json",
+                        no_redirect: 1,
+                        skip_disambig: 1,
+                    },
+                    function(data) {
+                        abstract = data.AbstractText;
+                        if (abstract){
+                            $("#header").html(title);
+                            $("#zci").html(abstract);
+                            $("#content").fadeIn('slow');
+                        }
+                        else
+                            setText();
+                    }
+                );
+                setOperaContexts(title);
             }
         );
     }
@@ -45,7 +43,7 @@ window.addEventListener('load', function() {
             var sd = opera.contexts.speeddial;
 
             sd.url = url;
-            sd.title = title;
+            sd.title = "DuckDuckGo: " + title;
         }
     }
 
@@ -56,6 +54,6 @@ window.addEventListener('load', function() {
             setText();
         });
 
-    }, 8000);
+    }, 10000);
 
 }, false);
